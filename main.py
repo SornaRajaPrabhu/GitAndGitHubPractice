ad5b7b386587abd55780d49a9f6c8c60b1208a90
PRABHU <datascientistprabhu@gmail.com>
2023-01-31 18:19:46 +0530

    Second Commit

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def anotherFunc():
+    print("Hi Prabhu")
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    anotherFunc()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
